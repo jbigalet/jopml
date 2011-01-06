9ae8da0169aed8ebe3f40bdc5fa0b512cfb20d9c
naellyn <naellyn@48c15e67-2d07-c7b5-5688-c1bc02a393f8>
2011-01-06 19:32:23 +0000

    oops

--- genproc.sh
+++ genproc.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-echo "> Netlist: netlists/proc.nl"
-echo "> Input (netlists/proc.nl.input) :"
-cat netlists/proc.nl.input | tail -n +2
-echo "> Simulation:"
-java -jar simulator/dist/JOP_Simulator.jar $* -brief -printram -f netlists/proc.nl.input -ram program_rom=./prog.rom netlists/proc.nl
+SDIR=$0
+SORIG=`pwd`
+S=$1
+cd `dirname $SDIR`
+echo "> Building proc"
+make clean > /dev/null
+make > /dev/null
+echo "> Generating netlist."
+circuits/proc $* netlists/proc.nl
+echo "> Netlist: netlists/$S.nl"
+cd $SORIG
