1cb75a922c11b80e5f4f689e4dcdeb1bb307e03a
naellyn <naellyn@48c15e67-2d07-c7b5-5688-c1bc02a393f8>
2011-01-10 22:00:53 +0000

    remove trunk

--- trunk/mips.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python asm/mips2as.py $2 > $2.as
-python asm/as.py $1 $2.as > prog.rom
--- trunk/runproc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "> Netlist: netlists/proc.nl"
-echo "> Input (netlists/proc.nl.input) :"
-cat netlists/proc.nl.input | tail -n +2
-echo "> Simulation:"
-java -jar simulator/dist/JOP_Simulator.jar $*  -printram -f netlists/proc.nl.input -ram program_rom=./prog.rom netlists/proc.nl
